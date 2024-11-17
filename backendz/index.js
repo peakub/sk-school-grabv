a2408c6534c5fdadd3def7fb44dc813cdb9e9bbf
peakub <noppakorn.rattanawan@gmai.com>
2024-11-17 10:44:27 +0700

    update

--- backendz/index.js
+++ backendz/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const app = express()
 require("dotenv").config();
-const port = process.env.PORT;
 const mongoDB = require("./db")
+
+const path = require('path')
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
@@ -17,6 +18,12 @@ app.use('/api', require("./Routes/OrderData"));
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+app.use(express.static(path.join(__dirname, '../build')))
+
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../build/index.html')))
+
+const port = process.env.PORT;
 
 app.listen(port, () => {
   console.log(`Jaopea listening on http://localhost:${port}`)
